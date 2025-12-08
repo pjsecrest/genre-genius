1c12e721c5253c5ef8e40865af0df6344bb917dd
Parker Secrest <pjsecrest@comcast.net>
2025-12-08 08:39:57 -0700

    update data transform

--- main.py
+++ main.py
@@ -58,19 +58,59 @@ def transformData(urls):
     # TODO: Potentially need to extract genre id -> labels, track_genres and track_genres_all are lists of genre_ids
     labels = tracks[['track_id', 'track_genre_top', 'track_genres', 'track_genres_all']]
     labels = labels.set_index('track_id')
+    
+    tracks = tracks.set_index('track_id')
     # print(tracks.head()) 
     # print(features.head())
     # print(labels.head())
     
+    # transform labels to integer ids 
+    # genres = pd.read_csv('./fma_metadata/genres.csv')
+    genres = pd.read_csv("./fma_metadata/genres.csv")
+    genre_title_id_map = dict(zip(genres['title'], genres['genre_id']))
+
+    labels['track_genre_top_id'] = labels['track_genre_top'].map(genre_title_id_map)
+    
     return features, labels, tracks
     
-def loadData():
-    pass
+# get training, test, and validation splits
+def loadData(features, labels, tracks):
+    # merge
+    data = features.join(labels, how='inner')
+    data = data.join(tracks[['set_split']], how='inner')
+    
+    # # split
+    X = data[features.columns]
+    y = data['track_genre_top_id']
+
+    train_idx = data['set_split'] == 'training'
+    val_idx = data['set_split'] == 'validation'
+    test_idx = data['set_split'] == 'test'
+    
+    X_train = X[train_idx]
+    X_val = X[val_idx]
+    X_test = X[test_idx]
+    
+    y_train = y[train_idx]
+    y_val = y[val_idx]
+    y_test = y[test_idx]
+    
+    print(f'X_train: \n {X_train.head()}')
+    print(f'y_train: \n {y_train.head()}')
+
+
     
 def main():
     urls = ['./fma_metadata/features.csv', './fma_metadata/tracks.csv']
+    
+    features, labels, tracks = transformData(urls) 
 
-    transformData(urls)
+    # print(features.head())
+    # print(labels.head())
+    # print(tracks.head())
+    
+    loadData(features, labels, tracks)
+
     
 if __name__ == "__main__":
     main()
